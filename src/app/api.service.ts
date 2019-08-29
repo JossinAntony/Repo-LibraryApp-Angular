1833d3420819292f7f2a788eb7c0962f6df86996
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-29 15:33:38 +0530

    icors ploicy updated.

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  retrieveBooks(){
+    return this.http.get('http://libraryapp-ict.herokuapp.com/retrieveBooksAPI');
+  }
+
 }
