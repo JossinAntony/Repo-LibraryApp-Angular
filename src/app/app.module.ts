1aba8d259609130f02cdedd8b083ce58526464ee
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-29 14:31:44 +0530

    nav-bar added.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms';
+
+import {HttpClientModule} from '@angular/common/http';
+import {Routes, RouterModule} from '@angular/router';
+
 
 import { AppComponent } from './app.component';
 import { ViewBooksComponent } from './view-books/view-books.component';
@@ -7,6 +12,14 @@ import { ViewAuthorsComponent } from './view-authors/view-authors.component';
 import { AddBooksComponent } from './add-books/add-books.component';
 import { AddAuthorsComponent } from './add-authors/add-authors.component';
 import { NavbarComponent } from './navbar/navbar.component';
+
+
+const appRoutes:Routes=[
+  {path:'',component:ViewBooksComponent},
+  {path:'viewAuthors',component:ViewAuthorsComponent},
+  {path:'addBook',component:AddBooksComponent},
+  {path:'addAuthor',component:AddAuthorsComponent}
+]
 
 @NgModule({
   declarations: [
@@ -18,9 +31,12 @@ import { NavbarComponent } from './navbar/navbar.component';
     NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,NavbarComponent]
 })
 export class AppModule { }
